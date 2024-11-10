2940dcffa14a416ba8650f41b964128218e9a5b1
Mancitiss <37582541+Mancitiss@users.noreply.github.com>
2024-11-10 12:01:13 -0800

    Update About.swift

--- Frame Grabber/Application/About.swift
+++ Frame Grabber/Application/About.swift
@@ -1,18 +1,18 @@
 import Foundation
 
 struct About {
-    static let storeURL = URL(string: "https://apps.apple.com/app/id1434703541")
-    static let storeReviewURL = URL(string: "https://apps.apple.com/app/id1434703541?action=write-review")
-    static let sourceCodeURL = URL(string: "https://github.com/arthurhammer/FrameGrabber")
-    static let contactAddress = "hi@arthurhammer.de"
-    static let inAppPurchaseIdentifier = "de.arthurhammer.framegrabber.thankyou"
+    static let storeURL = URL(string: "") // https://apps.apple.com/app/id1434703541
+    static let storeReviewURL = URL(string: "") // https://apps.apple.com/app/id1434703541?action=write-review"
+    static let sourceCodeURL = URL(string: "https://github.com/Mancitiss/FrameGrabber") // https://github.com/arthurhammer/FrameGrabber
+    static let contactAddress = "vovuongthanhtu@gmail.com" // hi@arthurhammer.de
+    static let inAppPurchaseIdentifier = "" // de.arthurhammer.framegrabber.thankyou
 }
 
 extension About {
 
     struct PrivacyPolicy {
-        static let en = URL(string: "https://framegrabberapp.com/privacy/")
-        static let de = URL(string: "https://framegrabberapp.com/datenschutz/")
+        static let en = URL(string: "") // https://framegrabberapp.com/privacy/
+        static let de = URL(string: "") // https://framegrabberapp.com/datenschutz/
 
         static var preferred: URL? {
             let language = Bundle.main.preferredLocalizations.first
